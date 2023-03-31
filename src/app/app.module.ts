1b384c94f67f41d905f991dc386c3225506b1afc
Jose Ignacio Santa Cruz G <j@santa.cl>
2023-03-31 09:59:46 -0300

    feat ADD initial commit
    Signed-off-by: Jose Ignacio Santa Cruz G <j@santa.cl>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { GlobalSignalService, GLOBAL_SIGNAL_SERVICE } from './services/global-signal.service';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [ AppComponent ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
-  providers: [],
-  bootstrap: [AppComponent],
+  providers: [
+    CommonModule,
+    BrowserModule,
+    provideHttpClient(),
+    // {
+    //   provide: GLOBAL_SIGNAL_SERVICE,
+    //   useClass: GlobalSignalService
+    // }
+  ],
+  bootstrap: [ AppComponent ],
 })
 export class AppModule {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'home',
+    redirectTo: '',
+  },
+  {
+    path: 'default',
+    loadChildren: () => import('./pages/default/default.module').then(m => m.DefaultModule)
+  },
+  {
+    path: 'on-push',
+    loadChildren: () => import('./pages/on-push/on-push.module').then(m => m.OnPushModule)
+  },
+  {
+    path: 'no-signals',
+    loadChildren: () => import('./pages/no-signals/no-signals.module').then(m => m.NoSignalsModule)
+  },
+  {
+    path: 'with-signals',
+    loadChildren: () => import('./pages/with-signals/with-signals.module').then(m => m.WithSignalsModule)
+  }
+];
